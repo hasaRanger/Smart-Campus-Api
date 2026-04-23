fc9f14d7f9fccf5ad55f952bda1789f0198b13af
Nadeesha Hasaranga <hasaranganadeesha2004@gmail.com>
2026-04-23 17:54:50 +0530

    feat: register all resources, mappers, and filters in JAX-RS Application class

---
+++ src/main/java/com/smartcampus/smart_campus_api/SmartCampusApp.java
@@ -0,0 +1,43 @@
+package com.smartcampus.smart_campus_api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.smartcampus.smart_campus_api.resources.DiscoveryResource;
+import com.smartcampus.smart_campus_api.resources.RoomResource;
+import com.smartcampus.smart_campus_api.resources.SensorReadingResource;
+import com.smartcampus.smart_campus_api.resources.SensorResource;
+import com.smartcampus.smart_campus_api.filter.LoggingFilter;
+import com.smartcampus.smart_campus_api.exception.mapper.*;
+
+import org.glassfish.jersey.jackson.JacksonFeature;
+/**
+ * Name: K. K. Nadeesha Hasaranga
+ * ID: 20240675 / w2120076
+ */
+
+@ApplicationPath("/api/v1")
+public class SmartCampusApp extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+
+        // Resources
+        classes.add(DiscoveryResource.class);
+        classes.add(RoomResource.class);
+        classes.add(SensorResource.class);
+
+        // Exception Mappers
+        classes.add(RoomNotEmptyExceptionMapper.class);
+        classes.add(LinkedResourceNotFoundExceptionMapper.class);
+        classes.add(SensorUnavailableExceptionMapper.class);
+        classes.add(GlobalExceptionMapper.class);
+
+        // Filters
+        classes.add(LoggingFilter.class);
+
+        return classes;
+    }
+}
